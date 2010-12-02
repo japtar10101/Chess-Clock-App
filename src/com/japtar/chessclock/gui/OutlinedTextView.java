23196728a4bc67760dd0c2d649d27ce6dc3c244f
Taro Omiya <japtar10101@gmail.com>
2010-12-01 21:58:17 -0500

    Starting on OutlinedTextView.  Lets see how far I go...

---
+++ src/com/japtar/chessclock/gui/OutlinedTextView.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.japtar.chessclock.gui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * TODO: add a description
+ * @author japtar10101
+ */
+public class OutlinedTextView extends TextView {
+
+	
+	/* ===========================================================
+	 * Constants
+	 * =========================================================== */
+
+	/* ===========================================================
+	 * Members
+	 * =========================================================== */
+
+	/* ===========================================================
+	 * Constructors
+	 * =========================================================== */
+	/**
+	 * TODO: add a description
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public OutlinedTextView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * TODO: add a description
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public OutlinedTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * TODO: add a description
+	 * @param context
+	 * @param attrs
+	 * @param defStyle
+	 */
+	public OutlinedTextView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+	/* ===========================================================
+	 * Overrides
+	 * =========================================================== */
+
+	/* ===========================================================
+	 * Public Methods
+	 * =========================================================== */
+
+	/* ===========================================================
+	 * Getters
+	 * =========================================================== */
+
+	/* ===========================================================
+	 * Setters
+	 * =========================================================== */
+
+	/* ===========================================================
+	 * Private/Protected Methods
+	 * =========================================================== */
+
+}
