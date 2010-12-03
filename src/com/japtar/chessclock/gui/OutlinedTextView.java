c2966d1548264024bc1a030008141394e74822aa
Taro Omiya <japtar10101@gmail.com>
2010-12-03 01:12:43 -0500

    So far, the best approach yet.

--- src/com/japtar/chessclock/gui/OutlinedTextView.java
+++ src/com/japtar/chessclock/gui/OutlinedTextView.java
@@ -8,12 +8,8 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.Gravity;
-import android.view.View;
-import android.view.ViewParent;
 import android.widget.TextView;
 
 import com.japtar.chessclock.R;
@@ -26,14 +22,11 @@ public class OutlinedTextView extends TextView {
 	 * Constants
 	 * =========================================================== */
 	private static final float OUTLINE_PROPORTION = 0.1f;
-	private static final Point ORIGIN = new Point();
 	
 	/* ===========================================================
 	 * Members
 	 * =========================================================== */
 	private final Paint mStrokePaint = new Paint();
-	private final Rect mTextBounds = new Rect();
-	private final Rect mTempBounds = new Rect();
 	private int mOutlineColor = Color.TRANSPARENT;
 	
 	/* ===========================================================
@@ -61,6 +54,7 @@ public class OutlinedTextView extends TextView {
     protected void onDraw(Canvas canvas) {
 		// Get the text to print
         final float textSize = super.getTextSize();
+        final String text = super.getText().toString();
         
         // setup stroke
         mStrokePaint.setColor(mOutlineColor);
@@ -70,35 +64,14 @@ public class OutlinedTextView extends TextView {
         mStrokePaint.setTypeface(super.getTypeface());
         
         // Figure out the drawing coordinates
-        this.updateBounds();
+        //mStrokePaint.getTextBounds(text, 0, text.length(), mTextBounds);
         
         // draw everything
-		canvas.drawText(super.getText().toString(),
-				mTextBounds.centerX(), mTextBounds.centerY(),
+		canvas.drawText(text,
+				super.getWidth() * 0.5f, super.getBottom() * 0.5f,
 				mStrokePaint);
 		super.onDraw(canvas);
     }
-	
-	/* ===========================================================
-	 * Getters
-	 * =========================================================== */
-	/**
-	 * @return {@link mOutlineColor}
-	 */
-	public int getOutlineColor() {
-		return mOutlineColor;
-	}
-	
-	/* ===========================================================
-	 * Setters
-	 * =========================================================== */
-	/**
-	 * @param outlineColor sets {@link mOutlineColor}
-	 */
-	public void setOutlineColor(int outlineColor) {
-		mOutlineColor = outlineColor;
-		super.invalidate();
-	}
 	
 	/* ===========================================================
 	 * Private/Protected Methods
@@ -114,35 +87,6 @@ public class OutlinedTextView extends TextView {
         mOutlineColor = array.getColor(
         		R.styleable.OutlinedTextView_outlineColor, 0x00000000);
         array.recycle(); 
-	}
-	private final void updateBounds() {
-		// Reset Text Bounds to default positions
-		mTextBounds.set(0, 0, 0, 0);
-		
-		// Grab the current child and parent
-		View child = this;
-		ViewParent parent = super.getParent();
-		
-		// Grab the bounds of each 
-		while((parent != null) && (child != null) && (parent != child)) {
-			// Grab the coordinates into a temporary rectangle
-			parent.getChildVisibleRect(child, mTempBounds, ORIGIN);
-			
-			// Increment the boundary coordinates
-			mTextBounds.left += mTempBounds.left;
-			mTextBounds.top += mTempBounds.top;
-			mTextBounds.right += mTempBounds.right;
-			mTextBounds.bottom += mTempBounds.bottom;
-			
-			// Attempt to convert the parent into a View
-			child = null;
-			if(parent instanceof View) {
-				child = (View) parent;
-			}
-			
-			// Get the next parent in the hierarchy
-			parent = parent.getParent();
-		}
 		
 		// Force this text label to be centered
 		super.setGravity(Gravity.CENTER_HORIZONTAL);
