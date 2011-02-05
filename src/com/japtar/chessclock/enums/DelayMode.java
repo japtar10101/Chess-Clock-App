ef0565c1117a026c9ba6c2724e2496b1b7d3c8c2
Taro Omiya <japtar10101@gmail.com>
2011-02-05 10:11:02 -0500

    Updating the license to include the MIT license

--- src/com/japtar/chessclock/enums/DelayMode.java
+++ src/com/japtar/chessclock/enums/DelayMode.java
@@ -1,14 +1,28 @@
 /**
- * <p>
- * Package of enumerators
- * </p>
+ * Chess Clock App
+ * https://github.com/japtar10101/Chess-Clock-App
  * 
- * <hr/>
+ * The MIT License
  * 
- * <p>
- * Chess Clock App 
- * Copyright 2011 Taro Omiya
- * </p>
+ * Copyright (c) 2011 Taro Omiya
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package com.japtar.chessclock.enums;
 
